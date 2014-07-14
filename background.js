f45fa2eb768b753e7d543706af970e5d8d8826e7
Dan Nuttall <pixelblend@gmail.com>
2014-07-14 15:12:45 +0100

    Find helper app, send out subscription request for messages

--- background.js
+++ background.js
@@ -1,8 +1,27 @@
-console.log('background mode');
+var services = [],
+    helperApp;
 
-var services = [];
+if(!helperApp) {
+  connectToHelperApp();
+}
 
 chrome.runtime.onMessageExternal.addListener(function (message) {
-  console.log('message received from app', message);
+  console.log("message received from app", message);
   services = message;
 });
+
+function connectToHelperApp() {
+  chrome.management.getAll(function(results) {
+    var matched = results.filter(function(r) {
+      return r.shortName == "radiodan-disco";
+    });
+
+    if(matched.length > 0) {
+      helperApp = matched[0].id;
+      console.log("sending to", helperApp);
+      chrome.runtime.sendMessage(helperApp, {subscribe:true});
+    } else {
+      console.error("cannot find helper app");
+    }
+  });
+}
