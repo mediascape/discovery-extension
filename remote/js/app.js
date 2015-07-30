938dd1bc780c6470c89797bb992dae796e1f6528
Andrew Nicolaou <andrew.nicolaou@bbc.co.uk>
2015-07-30 14:29:34 +0100

    Enable various player functions

--- remote/js/app.js
+++ remote/js/app.js
@@ -49,6 +49,7 @@ function buildUiWithPlayer(player) {
     debug: true
   });
 
+  // Set the name of the current device
   view.set('state.name', player.name);
 
   // view.observe('currentPlayer', function (newValue, oldValue, obj) {
@@ -65,6 +66,7 @@ function buildUiWithPlayer(player) {
   //   search(newValue);
   // });
 
+  // Triggered when a radio stream is picked from the list
   view.on('stream', function (evt) {
     evt.original.preventDefault();
     var url = evt.node.href;
@@ -73,41 +75,48 @@ function buildUiWithPlayer(player) {
       .then(currentPlayer.play);
   });
 
-  // view.on('next', function (evt) { evt.context.currentPlayer.nextTrack(); });
-  // view.on('previous', function (evt) { evt.context.currentPlayer.previousTrack(); });
-  //
-  // view.on('clear', function (evt) {
-  //   evt.context.currentPlayer.clearPlaylist();
-  // });
+  view.on('next', function (evt) { currentPlayer.nextTrack(); });
+  view.on('previous', function (evt) { currentPlayer.previousTrack(); });
 
-  // view.on('remove', function (evt) {
-  //   var pos = evt.context.Pos;
-  //   removeFromPlaylist(pos);
-  // });
-  //
-  // view.on('add-direct', function (evt) {
-  //   var file = evt.context.file;
-  //   if (file) {
-  //     addToPlaylist(file);
-  //     this.set('file', '');
-  //   }
-  // });
-  //
-  // view.on('add', function (evt) {
-  //   var file = evt.context.file;
-  //   addToPlaylist(file);
-  // });
-  //
-  // view.observe('volume', function (newValue, oldValue) {
-  //   console.log('ui volume changed', newValue, oldValue);
-  //   var firstSetting = newValue === null || oldValue === null;
-  //   // Only send updates if change is from UI
-  //   if (!firstSetting) {
-  //     console.log('send state update', newValue);
-  //     setVolume(newValue);
-  //   }
-  // });
-  //
+  // Clear playlist
+  view.on('clear', function (evt) {
+    currentPlayer.clearPlaylist();
+  });
+
+  view.on('remove', function (evt) {
+    var pos = evt.context.Pos;
+    currentPlayer.removeFromPlaylist(pos);
+  });
+
+  view.on('add-direct', function (evt) {
+    var file = evt.context.file;
+    if (file) {
+      currentPlayer.addToPlaylist(file);
+
+      // Empty file input field in UI
+      view.set('file', '');
+    }
+  });
+
+  view.on('add', function (evt) {
+    var file = evt.context.file;
+    currentPlayer.addToPlaylist(file);
+  });
+
+  // When the volume field changes, check whether to update
+  // the player volume
+  view.observe('state.volume', function (newValue, oldValue) {
+    console.log('ui volume changed from %o to %o ', oldValue, newValue);
+    var firstSetting = newValue === null || oldValue === null;
+    // Only send updates if change is from UI
+    if (!firstSetting) {
+      console.log('update volume to ', newValue);
+      currentPlayer.setVolume(newValue);
+    }
+  });
+
+  // Play or pause depending on what the current
+  // player state is
   view.on('play-pause', function handlePlayPause(evt) {
     // Get the current button state
     var currentState = evt.context.state.playback;
@@ -117,7 +126,7 @@ function buildUiWithPlayer(player) {
       currentPlayer.play();
     }
   });
-  //
+
   // document.addEventListener('searchresults', function (evt) {
   //   view.set('search.results', evt.results);
   // });
@@ -164,130 +173,50 @@ function fetchAndUpdateInitialState(player, view) {
       }
 
       if (status.player.state) {
-        // playerSpec.state = status.player.state;
         view.set('state.playback', status.player.state);
       }
 
+      if (status.player.volume) {
+        view.set('state.volume', status.player.volume);
+      }
+
       if (status.player.song) {
-        // playerSpec.current = status.player.song;
+        view.set('state.current', status.player.song);
       }
 
       if (status.player.nextsong) {
-        // playerSpec.next = status.player.nextsong;
+        view.set('state.next', status.player.nextsong);
       }
     });
 }
 
 function addPlayerStateChangeListeners(player, view) {
   console.log('adding listeners to player: ', player);
-  player.on('player', function () {console.log('player'); });
-  // player.on('player', function (info) {
-  //   console.log('info', info);
-  //
-  //   if (info.state) {
-  //     // playerSpec.state = info.state;
-  //     view.set('state.playback', info.state);
-  //   }
-  //
-  //   if (info.song) {
-  //     // playerSpec.current = info.song;
-  //     view.set('state.current', info.song);
-  //   }
-  //
-  //   if (info.nextsong) {
-  //     // playerSpec.next = info.nextsong;
-  //     view.set('state.next', info.nextsong);
-  //   }
-  //
-  //   // view.update();
-  // });
-}
 
-function createAndAttachPlayer(playerSpec) {
-
-  window.ui = window.ui || {};
-
-  var player = window.radio.player.create(playerSpec.id)
-
-  playerSpec.player = player;
-  playerSpec.playlist = [];
-
-  playerSpec.player.on('playlist', function (newPlaylist) {
-    console.log('playlist', newPlaylist, playerSpec);
-    var local = playerSpec.playlist;
-
-    local.length = 0;
-    playerSpec.playlist.push.apply(local, newPlaylist);
-    view.update();
-  });
-
-  window.ui.audio  = window.radio.audio.create('default');
-
-  // Get status to do an initial update of
-  // the user interface
-  player.status()
-    .then(function (status) {
-      console.log('player.status', status);
-
-      // if (status.playlist) {
-      //   playerSpec.playlist = status.playlist;
-      // }
-      //
-      // if (status.player.state) {
-      //   playerSpec.state = status.player.state;
-      // }
-      //
-      // if (status.player.song) {
-      //   playerSpec.current = status.player.song;
-      // }
-      //
-      // if (status.player.nextsong) {
-      //   playerSpec.next = status.player.nextsong;
-      // }
-
-      console.log('update view');
-      view.update();
-    });
-
-  /*
-     Listen for general player state changes
-     */
   player.on('player', function (info) {
-    console.log('info', info);
+    console.log('player', info);
 
     if (info.state) {
-      playerSpec.state = info.state;
+      view.set('state.playback', info.state);
     }
 
     if (info.song) {
-      playerSpec.current = info.song;
+      view.set('state.current', info.song);
     }
 
     if (info.nextsong) {
-      playerSpec.next = info.nextsong;
+      view.set('state.next', info.nextsong);
     }
-
-    view.update();
   });
 
-  window.ui.audio.status()
-    .then(function (status) {
-      var currentVolume = view.get('volume');
-      console.log('audio.status', currentVolume, status);
-      if (status.volume) {
-        if (currentVolume !== status.volume) {
-          view.set('volume', status.volume);
-        }
-      }
-    });
+  player.on('volume', function (info) {
+    var volume = parseInt(info.volume, 10);
+    console.log('volume', volume);
+    view.set('state.volume', volume);
+  });
 
-  /*
-     If the player volume is changed elsewhere in the
-     system, change the position of the slider
-     to match the new volume
-     */
-  window.ui.audio.on('volume', function(content) {
-    console.log('Volume has changed to ', content.value);
-    view.set('volume', content.value);
+  player.on('playlist', function (playlist) {
+    console.log('playlist', playlist);
+    view.set('state.playlist', playlist);
   });
 }
