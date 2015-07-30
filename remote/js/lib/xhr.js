f63700509b617d433c0898ae6b30a39f0588e4aa
Andrew Nicolaou <andrew.nicolaou@bbc.co.uk>
2015-07-30 14:58:25 +0100

    Remove unused XHR functions

--- remote/js/lib/xhr.js
+++ remote/js/lib/xhr.js
@@ -46,37 +46,3 @@ var xhr = (function () {
 
   return request;
 })();
-
-function getJSON(url, success) {
-  var xhr = createCORSRequest('GET', url);
-  xhr.withCredentials = true;
-  xhr.onload = function() {
-   var json = JSON.parse(xhr.responseText);
-   success(json);
-  };
-  xhr.send();
-}
-
-function createCORSRequest(method, url) {
-  var xhr = new XMLHttpRequest();
-  if ("withCredentials" in xhr) {
-
-    // Check if the XMLHttpRequest object has a "withCredentials" property.
-    // "withCredentials" only exists on XMLHTTPRequest2 objects.
-    xhr.open(method, url, true);
-
-  } else if (typeof XDomainRequest != "undefined") {
-
-    // Otherwise, check if XDomainRequest.
-    // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
-    xhr = new XDomainRequest();
-    xhr.open(method, url);
-
-  } else {
-
-    // Otherwise, CORS is not supported by the browser.
-    xhr = null;
-
-  }
-  return xhr;
-}
